c156f7ac2d6e67188e22f1f34cc60f81f6fe84de
meetmuaz <muazbalogun97@gmail.com>
2023-12-16 14:32:58 +0100

    0001

--- isPowerOfTwo-test.js
+++ isPowerOfTwo-test.js
@@ -1,19 +1,33 @@
 const print = console.log;
 
+// function isPowerOfTwo(n) {
+//     if(n < 1){
+//         return false;
+//     }
+//     while (n > 1) {
+//         if(n % 2 !== 0){
+//             return false;
+//         }
+//         n = n / 2;
+//     }
+//     return true;
+// }
+
+// Big-O = O(logn) 
+// IT IS LOGRITHMIC
+
+
 function isPowerOfTwo(n) {
     if(n < 1){
         return false;
     }
-    while (n > 1) {
-        if(n % 2 !== 0){
-            return false;
-        }
-        n = n / 2;
-    }
-    return true;
+
+    return (n & (n -1)) === 0;
 }
+
+// Big-O = O(n) 
+// IT IS LINEAR
 
 print(isPowerOfTwo(1));
 print(isPowerOfTwo(2));
 print(isPowerOfTwo(5));
-print(isPowerOfTwo(16));
