598b20d18bd337cb0668226401063705152ca7e5
meetmuaz <muazbalogun97@gmail.com>
2024-01-15 02:12:53 +0100

    length corrected

--- linear-search.js
+++ linear-search.js
@@ -1,7 +1,7 @@
 const print = console.log;
 
 function linearSearch(arr, target){
-    for (let i = 0; i < arr.lenght; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if(arr[i] === target) {
             return i
         }
